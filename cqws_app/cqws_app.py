c24bca1ec55e95b7f2d990c985d855ffc778c370
RUCO13 <ruizoscar.1393@gmail.com>
2023-04-05 17:05:08 -0600

    Update

--- cqws_app/cqws_app.py
+++ cqws_app/cqws_app.py
@@ -1,2 +1,19 @@
-import numpy as np
-import streamlit as st 
+import os
+import sentry_sdk
+import streamlit as st
+
+if os.path.isfile(".streamlit/secrets.toml"):
+    if 'sentry_url' in st.secrets:
+        sentry_sdk.init(
+            st.secrets['sentry_url'],
+            # Set traces_sample_rate to 1.0 to capture 100%
+            # of transactions for performance monitoring.
+            # We recommend adjusting this value in production.
+            traces_sample_rate=0.001,
+        )
+    else:
+        print('sentry not running')
+else:
+    print('Ok!')
+    
+    
