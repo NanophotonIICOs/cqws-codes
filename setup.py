0396022db0a904dfad2534fbc09ab7367eec945b
RUCO13 <ruizoscar.1393@gmail.com>
2023-04-22 16:43:56 -0600

    Update the setup.py and notify me if there is an error.

--- setup.py
+++ setup.py
@@ -1,5 +1,37 @@
 from setuptools import setup, find_packages
 
+# setup(
+#     name='cqws',
+#     version='1.0',
+#     description='Solver of Quantum Wells trough Finite Difference Method, with arbitrary potential',
+#     author='O. Ruiz-Cigarrillo et al.',
+#     author_email='ruizoscar.1393@gmail.com',
+#     url='https://github.com/NanophotonIICOs/cqws-codes.git',
+#     packages=find_packages('cqws'),
+#     package_dir={'': 'cqws'},
+#     install_requires=[
+#         'numpy',
+#         'scipy',
+#         'matplotlib',
+#         'tqdm',
+#         'numba',
+#         'scipy',
+#         'Ipython',
+#         'tabulate',
+#         'pandas',
+#         'h5py',
+#         'ipykernel',
+#         'astropy',
+#         'lumispy',
+#         'specutils',
+#         'peakutils',
+#         'Sphinx',
+#         'streamlit',
+#         'julia',
+#         'plotly==5.14.1'
+#     ],
+# )
+
 setup(
     name='cqws',
     version='1.0',
@@ -7,8 +39,7 @@ setup(
     author='O. Ruiz-Cigarrillo et al.',
     author_email='ruizoscar.1393@gmail.com',
     url='https://github.com/NanophotonIICOs/cqws-codes.git',
-    packages=find_packages('cqws'),
-    package_dir={'': 'cqws'},
+    packages=['cqws'],
     install_requires=[
         'numpy',
         'scipy',
